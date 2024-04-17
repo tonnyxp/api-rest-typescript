04fffa59bad915a5706e54ec6283306a62168eae
Antonio Alvarado <aalvarado@grupobeh.com>
2024-04-17 14:40:48 -0600

    Actualizar seeder de usuarios para incluir roles

--- src/database/seeders/users.seeder.ts
+++ src/database/seeders/users.seeder.ts
@@ -1,21 +1,41 @@
 import bcrypt from "bcryptjs";
-import User from "../../models/users.model";
+import RoleModel from "../../models/roles.model";
+import UserModel from "../../models/users.model";
 
-const users = [
+enum ROLE_TYPES {
+  ADMINISTRATOR = "admin",
+  USER = "user",
+}
+
+const roles = [{ name: ROLE_TYPES.ADMINISTRATOR }, { name: ROLE_TYPES.USER }];
+
+const findRoleId = async (name: string) => {
+  const data = await RoleModel.findOne({ where: { name } });
+  if (!data) {
+    throw new Error(`Role ${name} not found`);
+  }
+
+  return data.roleId;
+};
+
+const users = async () => [
   {
     name: "Administrator",
     email: "admin@example.com",
     password: bcrypt.hashSync("admin123", 8),
+    verified: true,
+    roleId: await findRoleId(ROLE_TYPES.ADMINISTRATOR),
   },
   {
     name: "Demo",
     email: "demo@example.com",
-    password: bcrypt.hashSync("demo123", 8),
+    password: bcrypt.hashSync("temporal", 8),
+    roleId: await findRoleId(ROLE_TYPES.USER),
   },
 ];
 
 export const seedUsers = async () => {
   console.log("Seeding users");
-  await User.truncate();
-  await User.bulkCreate(users);
+  await RoleModel.bulkCreate(roles);
+  await UserModel.bulkCreate(await users());
 };
